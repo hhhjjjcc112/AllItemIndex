5dab3b1fc48e56139420620a053e56bb2fd7d46d
hhhjjjcc <hjch142857@126.com>
2024-10-11 22:22:51 +0800

    draw some pancakes

--- src/main/java/com/hjc/allitemindex/repository/IndexJsonLoader.java
+++ src/main/java/com/hjc/allitemindex/repository/IndexJsonLoader.java
@@ -4,7 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.hjc.allitemindex.model.ItemInfo;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -15,6 +18,15 @@ import java.util.List;
 public class IndexJsonLoader {
 
     private static final Path root = FabricLoader.getInstance().getConfigDir().resolve("allitemindex");
+    static {
+        if (!Files.exists(root)) {
+            try {
+                Files.createDirectories(root);
+            } catch (IOException e) {
+                System.err.println("Failed to create directory " + root);
+            }
+        }
+    }
     private static final Path indexFile = root.resolve("index.json");
     // 标识是否加载
     private static final ItemInfo infos = new ItemInfo();
@@ -24,19 +36,29 @@ public class IndexJsonLoader {
     private static final Gson gson = new GsonBuilder().serializeNulls().create();
 
 
-    public static ItemInfo getInfos() {
+    public static ItemInfo getInfos(CommandContext<ServerCommandSource> context) {
         synchronized (IndexJsonLoader.class) {
             if(!loaded) {
                 loaded = loadFromLocal();
+                alertIfFailed(context);
             }
             return infos;
         }
     }
 
-    public static boolean reload() {
+    public static boolean reload(CommandContext<ServerCommandSource> context) {
         synchronized (IndexJsonLoader.class) {
             loaded = loadFromLocal();
+            alertIfFailed(context);
             return loaded;
+        }
+    }
+
+    private static void alertIfFailed(CommandContext<ServerCommandSource> context) {
+        if(!loaded) {
+            ServerCommandSource source = context.getSource();
+            var server = source.getServer();
+            server.sendMessage(Text.literal("failed to load index.json. Please check if the file exists."));
         }
     }
 
